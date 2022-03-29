25a173c5b39f8e5458d556fe88cc325124cfe976
ciptakelvin <ciptakelvin@gmail.com>
2022-03-29 15:54:49 +0700

    Add News Section

---
+++ j/global.js
@@ -0,0 +1,20 @@
+news=[
+    {
+        title:"Logo Baru SarangKoding!",
+        date:"29/03/2022",
+        content:"Dengan adanya logo baru ini, kami berharap kedepannya Sarang Koding semakin semangat dalam membuat game baru.",
+        cover:"i/logo-sarangkoding.webp"
+    },
+    {
+        title:"Kami Merilis Game At Midnight!",
+        date:"29/03/2022",
+        content:"Kami resmi merilis game baru khusus untuk android. Game ini bertipe platformer. Jelajahi dunia malam di At Midnight! Anda akan mendapat tantangan untuk melewati kegelapan di tengah malam. Anda harus terus mendapatkan cahaya untuk bertahan hidup. Tanpa cahaya yang Anda serap. Dunia akan diselimuti kegelapan.",
+        cover:"i/at-midnight-cover.webp"
+    },
+    {
+        title:"Pocong Escape, Game Uji Nyali Realistis",
+        date:"29/03/2022",
+        content:"Pocong adalah hantu dari Indonesia. Banyak cerita mistis tentangnya dan tidak sedikit yang takut. Itu adalah manusia mati yang terbungkus kain kafan. Anda akan mengumpulkan catatan untuk menemukan cara untuk keluar dari gedung tua. Anda harus bisa menghindari kejaran pocong. Hafalkan jalan yang telah Anda ambil dan temukan jalan keluarnya.",
+        cover:"i/pocong-escape-cover.png"
+    }
+]
--- j/index.js
+++ j/index.js
@@ -1,8 +1,14 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'text.txt', true);
-xhr.send();
+window.onload=function(){
 
-
-function loadDoc(){
-    alert(xhr.responseText);
+    var content="";
+    for(var n of news){
+        
+        content+="<div id='news-card'>";
+        content+="<b>"+n["title"]+"</b><br>";
+        content+="<p>"+n["date"]+"</p><br>";
+        content+="<img src='"+n["cover"]+"'></img><br>";
+        content+="<p>"+n["content"]+"</p><br>";
+        content+="</div>";
+    }
+    document.getElementById("index-news").innerHTML+=content;
 }
